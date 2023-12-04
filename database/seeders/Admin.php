613bacb2b65f16acedbbcc14473082fabfa1fecd
enrikcapistran <enric.capistran@gmail.com>
2023-12-04 08:55:35 -0700

    Corrección Users y sesiones separadas

--- database/seeders/Admin.php
+++ database/seeders/Admin.php
@@ -17,8 +17,8 @@ class Admin extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Administrador',
-            'email' => 'admin@ferretec.com',
+            'nombre' => 'Administrador2',
+            'email' => 'admin2@ferretec.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="register-page min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 py-12 px-4 sm:px-6 lg:px-8">
-        <div class="max-w-md w-full bg-white p-8 rounded-lg shadow-md">
+        <div class="max-w-md w-full bg-white p-5 rounded-lg shadow-md">
             <div>
                 
                 <h2 class="mt-6 text-center text-3xl font-extrabold text-black">
