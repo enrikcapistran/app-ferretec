f6212b93dcb34c5b0190a728cdeb66b4e19066a0
enrikcapistran <enric.capistran@gmail.com>
2023-12-07 12:39:09 -0700

    Fix a la superposicion de componentes  sobre navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,10 @@
         <div class="mx-auto mt-2 text-black-500 md:text-center lg:text-lg">
             Con La Calidad Y Precio Que Buscas
         </div>
-        <div class="flex flex-col items-center mt-12 text-center">
-            <span class="relative inline-flex w-full md:w-auto" style="z-index: 0;">
-                <a href="/productos" type="button" class="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 focus:outline-none">
-                    Ver Productos
-                </a>
+        <div class="mt-8">
+            <a href="/productos" type="button" class="px-6 py-3 text-base font-bold text-white bg-green-600 rounded-full hover:bg-green-500 focus:outline-none">
+                Ver Productos
+            </a>
         </div>
     </div>
     <!-- End Main Hero Content -->
@@ -63,15 +62,6 @@
                             Sabemos que nuestros clientes quieren productos eficientes, para ello, ofrecemos un amplio catálogo de productos de ferretería, modelos de última generación, materiales y herramientas de excelente calidad para tus proyectos.
                             <br>
                         </p>
-                        <div class="relative flex">
-                            <a href="#_" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-green-600 rounded-md sm:mb-0 hover:bg-green-700 sm:w-auto">
-                                Ver más
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <line x1="5" y1="12" x2="19" y2="12"></line>
-                                    <polyline points="12 5 19 12 12 19"></polyline>
-                                </svg>
-                            </a>
-                        </div>
                     </div>
                 </div>
                 <div class="w-full md:w-1/2">
