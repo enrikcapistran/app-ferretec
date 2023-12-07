ac0160ec38aa9994017ad237dec8bad99aa45284
Diego Cruz <18170307@itculiacan.edu.mx>
2023-12-06 20:05:08 -0700

    fix productos

--- resources/views/admin/productos/index.blade.php
+++ resources/views/admin/productos/index.blade.php
@@ -7,14 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
+
             <div class="flex justify-end m-2 p-2">
                 <a href="{{ route('admin.productos.create') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg
                 text-white">
                     Nuevo Producto
                 </a>
             </div>
-        
+
             <div class="relative overflow-x-auto">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -59,13 +59,10 @@
                             </td>
                             <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 <div class="flex space-x-2">
-                                    <a href="{{ route('admin.productos.edit', $producto->id) }}"
-                                    class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">
+                                    <a href="{{ route('admin.productos.edit', $producto->idProducto) }}" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">
                                         Editar
                                     </a>
-                                    <form class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white"
-                                    method="POST" action="{{ route('admin.productos.destroy', $producto->id) }}"
-                                    onsubmit="return confirm('¿Desea eliminar este producto?')">
+                                    <form class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white" method="POST" action="{{ route('admin.productos.destroy', $producto->idProducto) }}" onsubmit="return confirm('¿Desea eliminar este producto?')">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit">Eliminar</button>
@@ -74,10 +71,10 @@
                             </td>
                         </tr>
                         @endforeach
-                        
-                        
-                            
-                        
+
+
+
+
                     </tbody>
                 </table>
             </div>
