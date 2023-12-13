013e11f89c79a6e39fbd79e7fc69939d8c5f8a78
enrikcapistran <enric.capistran@gmail.com>
2023-12-13 11:47:03 -0700

    Autenticacion de cliente registrado y no registrado al momento de proceder al pago

--- resources/views/carrito/index.blade.php
+++ resources/views/carrito/index.blade.php
@@ -5,14 +5,14 @@
 
             @if (session('carrito') && count($carrito->getDetalles()) > 0)
             <div class="mt-8">
-                <table class="w-full text-left bg-white rounded-lg shadow-md">
-                    <thead class="bg-gray-300">
+                <table class="w-2/3 text-left bg-white rounded-lg shadow-md mx-auto">
+                    <thead class="bg-blue-800">
                         <tr>
-                            <th class="px-6 py-4 text-lg font-bold text-gray-600">Producto</th>
-                            <th class="px-6 py-4 text-lg font-bold text-gray-600">Precio</th>
-                            <th class="px-6 py-4 text-lg font-bold text-gray-600">Cantidad</th>
-                            <th class="px-6 py-4 text-lg font-bold text-gray-600">Total</th>
-                            <th class="px-6 py-4 text-lg font-bold text-gray-600">Acciones</th>
+                            <th class="px-6 py-4 text-lg font-bold text-white">Producto</th>
+                            <th class="px-6 py-4 text-lg font-bold text-white">Precio</th>
+                            <th class="px-6 py-4 text-lg font-bold text-white">Cantidad</th>
+                            <th class="px-6 py-4 text-lg font-bold text-white">Total</th>
+                            <th class="px-6 py-4 text-lg font-bold text-white">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -51,11 +51,19 @@
                     </tbody>
                 </table>
             </div>
-            <div class="flex justify-end mt-8">
-                <a href="{{ route('pago.step-one') }}" class="px-8 py-4 text-xl font-bold text-white bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none mr-10">
-                    Proceder al Pago
-                </a>
+            <div class="flex justify-end mt-8 w-2/3 mx-auto">
+                @auth
+                    <a href="{{ route('pago.step-one') }}" class="px-8 py-4 text-xl font-bold text-white bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none mr-10">
+                        Proceder al Pago
+                    </a>
+                @else
+                    <a href="{{ route('register') }}" class="px-8 py-4 text-xl font-bold text-white bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none mr-10">
+                        Proceder al Pago
+                    </a>
+                @endauth
             </div>
+
+            
             
             @else
             <p class="text-center text-lg mt-6">Tu carrito de compras está vacío.</p>
